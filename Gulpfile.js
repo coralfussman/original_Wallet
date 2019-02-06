7ddadecde1ba7ae12f3d51a7bc74d6c607f30e44
Sam Goldberg <sgoldber61@gmail.com>
2019-02-05 23:15:13 -0800

    gulp solution complete

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,49 @@
-var gulp = require('gulp');
-var source = require('vinyl-source-stream');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var reactify = require('reactify');
-var nodemon = require('gulp-nodemon');
+const gulp = require('gulp');
+const source = require('vinyl-source-stream');
+const browserify = require('browserify');
+const watchify = require('watchify');
+const babelify = require('babelify');
+const sassify = require('sassify');
+const nodemon = require('gulp-nodemon');
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
+const livereload = require('gulp-livereload');
+const path = require('path');
 
-gulp.task('serve', serve);
+const sourceFile = path.join(__dirname, '/client/index.js');
+const destFile = 'bundle.js';
+const destFolder = path.join(__dirname, 'build');
 
-// simple gulp task to start the server
-function serve() {
+gulp.task('browserify-prod', function () {
+  return browserify(sourceFile)
+  .transform(babelify, {presets: ['@babel/env', '@babel/react']})
+  .transform(sassify)
+  .bundle()
+  .pipe(source(destFile))
+  .pipe(buffer())
+  .pipe(uglify())
+  .pipe(gulp.dest(destFolder));
+});
+
+gulp.task('browserify-dev', function () {
+  const bundler = watchify(browserify({entries: [sourceFile], cache: {}}));
+  bundler.on('update', () => bundle());
+  bundler.on('error', console.error);
+  
+  function bundle() {
+    return bundler.transform(babelify, {presets: ['@babel/env', '@babel/react']})
+      .transform(sassify)
+      .bundle()
+      .pipe(source(destFile))
+      .pipe(gulp.dest(destFolder))
+  }
+  
+  return bundle();
+});
+
+gulp.task('serve', function () {
   nodemon({script: 'server/server.js'});
-}
+});
 
-// gulps default task is to call the serve task
-gulp.task('default', ['serve']);
+gulp.task('dev', ['browserify-dev', 'serve']);
+gulp.task('prod', ['browserify-prod']);
