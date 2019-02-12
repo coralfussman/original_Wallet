c12356d8aecfe596d7ceb326b9ef6954aebfcac2
Sam Goldberg <sgoldber61@gmail.com>
2019-02-11 16:54:59 -0800

    gulpfile hints

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,13 @@
 const gulp = require('gulp');
+const source = require('vinyl-source-stream');
+const browserify = require('browserify');
+const watchify = require('watchify');
+const babelify = require('babelify');
+const sassify = require('sassify');
+const nodemon = require('gulp-nodemon');
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
+const path = require('path');
 
 const sourceFile = path.join(__dirname, '/client/index.js');
 const destFile = 'browserify-bundle.js';
