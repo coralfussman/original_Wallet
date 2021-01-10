7b8c08c34b408409e43bcc4ddb2742d689e48cec
Matt Meigs <mattmeigs@gmail.com>
2021-01-10 15:37:35 -0500

    changed Extract-Text plugin to Mini-CSS plugin, changed webpack version to handle that change, added best practices to server.js and spellchecked the readme

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const leaderList = [
 ];
 
 app.get('/api/leaders', (req, res) => {
-  res.send(leaderList);
+  return res.status(200).send(leaderList);
 });
 */
 
@@ -20,7 +20,7 @@ app.get('/api/leaders', (req, res) => {
 app.use('/build', express.static(path.join(__dirname, '../build')));
 // serve index.html on the route '/'
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.listen(3000); //listens on port 3000 -> http://localhost:3000/
